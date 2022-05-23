7c89a34a7a74a91bd30105fc2e6a6cc4af34421b
nnngozi <nnngozi@gmail.com>
2022-05-23 23:31:02 +0000

    updated05/23

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./tools/Navbar.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			
+			<Navbar />
 		</div>
 	);
 };
