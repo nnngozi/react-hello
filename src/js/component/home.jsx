2d1b17418c7aaaf20375e1e7f1d868891f111a28
nnngozi <nnngozi@gmail.com>
2022-05-24 02:51:18 +0000

    updatednow

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,12 +3,16 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./tools/Navbar.jsx";
+import Jumbotron from "./tools/Jumbotron.jsx";
+import Cards from "./tools/Cards.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<Jumbotron />
+			<Cards />
 		</div>
 	);
 };
